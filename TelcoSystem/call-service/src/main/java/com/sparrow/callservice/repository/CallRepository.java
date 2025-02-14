38ebe0602b6116ab86c386da67721fd1f106f90b
Arison99 <harrisondaviinci@gmail.com>
2025-02-14 21:07:36 +0300

    Remove unused classes and configuration files to clean up the project structure

--- TelcoSystem/call-service/src/main/java/com/sparrow/callservice/repository/CallRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sparrow.callservice.repository;
-
-import com.sparrow.callservice.model.Call;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CallRepository extends JpaRepository<Call, Long> {
-}
