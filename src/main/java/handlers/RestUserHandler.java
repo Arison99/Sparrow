5d70a5914e92b1dff3526b55a0fd1213383012c0
Arison99 <harrisondaviinci@gmail.com>
2025-07-15 18:22:50 +0300

    Create RestUserHandler.java

---
+++ src/main/java/handlers/RestUserHandler.java
@@ -0,0 +1,20 @@
+package handlers;
+
+import adapters.BackendServiceJsonAdapter;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+
+public class RestUserHandler implements HttpHandler {
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        String response = BackendServiceJsonAdapter.getAllUsersJson();
+        exchange.getResponseHeaders().add("Content-Type", "application/json");
+        exchange.sendResponseHeaders(200, response.getBytes(StandardCharsets.UTF_8).length);
+        OutputStream os = exchange.getResponseBody();
+        os.write(response.getBytes(StandardCharsets.UTF_8));
+        os.close();
+    }
+}
