2212d159bb66983efe79562b561a78defdbd6f7a
Arison99 <harrisondaviinci@gmail.com>
2025-07-15 18:22:43 +0300

    Create RestInvestInsureHandler.java

---
+++ src/main/java/handlers/RestInvestInsureHandler.java
@@ -0,0 +1,22 @@
+package handlers;
+
+import adapters.BackendServiceInvestInsureAdapter;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+
+public class RestInvestInsureHandler implements HttpHandler {
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        // /api/investinsure?phone=...&action=invest|insure&amount=...
+        String query = exchange.getRequestURI().getQuery();
+        String result = BackendServiceInvestInsureAdapter.handleInvestInsureRequest(query);
+        exchange.getResponseHeaders().add("Content-Type", "application/json");
+        exchange.sendResponseHeaders(200, result.getBytes(StandardCharsets.UTF_8).length);
+        OutputStream os = exchange.getResponseBody();
+        os.write(result.getBytes(StandardCharsets.UTF_8));
+        os.close();
+    }
+}
