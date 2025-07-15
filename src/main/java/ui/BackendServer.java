7d34b0ccef42f5a89b9c154ee0fc00e6089fe1ea
Arison99 <harrisondaviinci@gmail.com>
2025-07-15 18:21:21 +0300

    Create BackendServer.java

---
+++ src/main/java/ui/BackendServer.java
@@ -0,0 +1,11 @@
+package ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendServer {
+    public static void main(String[] args) {
+        SpringApplication.run(BackendServer.class, args);
+    }
+}
