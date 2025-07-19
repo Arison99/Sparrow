764b7432df51942a215197231133be75e8cc1fbe
Arison99 <harrisondaviinci@gmail.com>
2025-07-19 21:51:42 +0300

    Update BackendServer.java

--- src/main/java/ui/BackendServer.java
+++ src/main/java/ui/BackendServer.java
@@ -3,7 +3,7 @@ package ui;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"ui", "gateway", "hardware", "core", "handlers", "adapters"})
 public class BackendServer {
     public static void main(String[] args) {
         SpringApplication.run(BackendServer.class, args);
